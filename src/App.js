d5b1616abddd81b83e03623a580d0b11c0a6b811
Gaurav Khatri <gaurav.khatri@polestarllp.com>
2021-10-26 22:10:00 +0530

    Added successfully

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Button, Form, Container, Header } from 'semantic-ui-react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [name, setName] = useState('');
+	const [age, setAge] = useState('');
+	const [salary, setSalary] = useState('');
+	const [hobby, setHobby] = useState('');
+
+	const handleSubmit = (e) => {
+		const objt = { name, age, salary, hobby };
+
+    console.log(objt);
+
+		axios
+			.post(
+				'https://sheet.best/api/sheets/c45f40e9-80c4-4439-ad16-48cdd199db9f',
+				objt
+			)
+			.then((response) => {
+				console.log(response);
+			})
+      .catch((err) => {
+        console.log(err);
+      });
+
+      setName('');
+      setAge('');
+      setSalary('');
+      setHobby('');
+	};
+
+	return (
+		<Container fluid className="container">
+			<Header as="h2">React google sheet</Header>
+			<Form className="form">
+				<Form.Field>
+					<label>Name</label>
+					<input
+						placeholder="Enter your Name"
+						onChange={(e) => setName(e.target.value)}
+					/>
+				</Form.Field>
+				<Form.Field>
+					<label>Age</label>
+					<input
+						placeholder="Enter your Age"
+						onChange={(e) => setAge(e.target.value)}
+					/>
+				</Form.Field>
+				<Form.Field>
+					<label>Salary</label>
+					<input
+						placeholder="Enter your Salary"
+						onChange={(e) => setSalary(e.target.value)}
+					/>
+				</Form.Field>
+				<Form.Field>
+					<label>Hobby</label>
+					<input
+						placeholder="Enter your Hobby"
+						onChange={(e) => setHobby(e.target.value)}
+					/>
+				</Form.Field>
+
+				<Button color="blue" type="submit" onClick={handleSubmit}>
+					Submit
+				</Button>
+			</Form>
+		</Container>
+	);
 }
 
 export default App;
